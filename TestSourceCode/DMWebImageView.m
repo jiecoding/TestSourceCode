10d8ef342ab404d459cd4abcb3f7da63c4316122
jiecoding <2015089414@qq.com>
2019-10-26 10:15:12 +0800

    多张图片测试

--- TestSourceCode/DMWebImageView.m
+++ TestSourceCode/DMWebImageView.m
@@ -16,6 +16,7 @@ static NSOperationQueue *cacheInQueue;
 
 - (void)dealloc
 {
+    
     [placeHolderImage release];
     [currentOperation release];
     [super dealloc];
@@ -57,7 +58,7 @@ static NSOperationQueue *cacheInQueue;
         }
         
         currentOperation = [[DMWebImageDownloadOperation alloc] initWithURL:url delegate:self];
-        [downloadQueue addOperation:currentOperation];
+       [downloadQueue addOperation:currentOperation];
     }
 }
 
@@ -91,8 +92,8 @@ static NSOperationQueue *cacheInQueue;
 
     return self;
 }
-
-- (void)main 
+//main方法的话，如果main方法执行完毕，那么整个operation就会从队列中被移除
+- (void)main
 {
     if (self.isCancelled)
     {
@@ -122,11 +123,20 @@ static NSOperationQueue *cacheInQueue;
     [imageCache storeImage:image forKey:cacheKey toDisk:NO];
 
     // Perform the cache-in in another operation queue in order to not block a download operation slot
+    //在另一个操作队列中执行缓存，以避免阻塞下载操作插槽
+    
+    //创建签名对象的时候不是使用NSMethodSignature这个类创建，而是方法属于谁就用谁来创建
+    //    NSMethodSignature*signature = [ViewController instanceMethodSignatureForSelector:@selector(sendMessageWithNumber:WithContent:)];
+    //1、创建NSInvocation对象
     NSInvocation *cacheInInvocation = [NSInvocation invocationWithMethodSignature:[[imageCache class] instanceMethodSignatureForSelector:@selector(storeImage:forKey:)]];
     [cacheInInvocation setTarget:imageCache];
     [cacheInInvocation setSelector:@selector(storeImage:forKey:)];
+    //注意：
+    //1.自定义的参数索引从2开始，0和1已经被self and _cmd占用了
+    //2.方法签名中保存的方法名称必须和调用的名称一致
     [cacheInInvocation setArgument:&image atIndex:2];
     [cacheInInvocation setArgument:&cacheKey atIndex:3];
+    //retain 所有参数，防止参数被释放dealloc
     [cacheInInvocation retainArguments];
     NSInvocationOperation *cacheInOperation = [[NSInvocationOperation alloc] initWithInvocation:cacheInInvocation];
     [cacheInQueue addOperation:cacheInOperation];
--- TestSourceCode/ViewController.m
+++ TestSourceCode/ViewController.m
@@ -15,11 +15,15 @@
  自己使用的图片视图控件
  */
 @property (weak, nonatomic) IBOutlet UIImageView *myImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *oneMyImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *threeMyImageView;
 
 /**
  第三方加载图片使用的图片视图控件
  */
 @property (weak, nonatomic) IBOutlet DMWebImageView *testImageView;
+@property (weak, nonatomic) IBOutlet DMWebImageView *leftTestImageView;
+@property (weak, nonatomic) IBOutlet DMWebImageView *rightTestImageView;
 
 @end
 
@@ -33,11 +37,23 @@
     NSData *imageData = [NSData dataWithContentsOfURL:imageUrl];
     //3.图片直接加载data二进制数据来获取展示图片
     self.myImageView.image = [UIImage imageWithData:imageData];
+    //多张图片,经常来回刷新，缓存的需求 更加迫不及待。不管是在加载速度还是流量上都要提升和节省很多。
+    self.oneMyImageView.image = [UIImage imageWithData:imageData];
+    self.threeMyImageView.image = [UIImage imageWithData:imageData];
+
     
     
     
+    
+    // 图片链接是一个地址的时候，sdwebimageview会怎么处理呢？
+    self.testImageView.tag = 2000;
     [self.testImageView setImageWithURL:[NSURL URLWithString:@"http://n.sinaimg.cn/edu/transform/20160505/pe7k-fxryhhu2274915.png"]];
-    
+    self.leftTestImageView.tag = 2001;
+    [self.leftTestImageView setImageWithURL:[NSURL URLWithString:@"http://n.sinaimg.cn/edu/transform/20160505/pe7k-fxryhhu2274915.png"]];
+    self.rightTestImageView.tag = 2002;
+    [self.rightTestImageView setImageWithURL:[NSURL URLWithString:@"http://n.sinaimg.cn/edu/transform/20160505/pe7k-fxryhhu2274915.png"]];
+   
+
     
 }
 
